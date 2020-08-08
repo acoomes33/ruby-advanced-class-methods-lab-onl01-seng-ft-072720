5b2c21a2df354b4875c872eb3b23d6e379c2ba19
Alan Coomes <acoomes1@cord.edu>
2020-08-08 01:16:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name 
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song 
+  end
+  
+  def self.find_by_name(name)
+    
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+  end
+  
+  def self.alphabetical
+    self.sort{|a,b| a<=>b}
+  end
+  
+  def self.new_from_filename(filename)
+    
+  end
+  
+  def self.create_from_filename(filename)
+    
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
